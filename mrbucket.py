10a32ebf47eee193bed0b33bdb481a0239f630da
Matthew Alford <matthew.alford@controlgroup.com>
2013-03-07 11:33:14 -0500

    added try except to make sure iam keys are in env variables

--- mrbucket.py
+++ mrbucket.py
@@ -5,10 +5,16 @@
 import boto
 import os
 import argparse
+import sys
 from hurry.filesize import size
 
-aws_access_key = os.environ["AWS_ACCESS_KEY"]
-aws_secret_access_key = os.environ["AWS_SECRET_ACCESS_KEY"]
+try:
+    aws_access_key = os.environ["AWS_ACCESS_KEY"]
+    aws_secret_access_key = os.environ["AWS_SECRET_ACCESS_KEY"]
+except KeyError:
+    print "you need to have the aws access key secret access key in you environnment variable"
+    sys.exit(1)
+
 conn = boto.connect_s3(aws_access_key, aws_secret_access_key)
 bucket = conn.lookup('akiaj5id5wjv45cdcy5acomhaystacksoftwarearq')
 
